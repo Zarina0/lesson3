fe759b2d161652e0ab4d12413be6c5b53614e996
Zarina0 <duulatbekovazaku@gmail.com>
2021-11-23 19:12:31 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+    }
+    public static void testmethod(int num){
+        int a =1;
+
+        if (7<10){
+
+            int b =2;
+
+        }
+        System.out.println({"int num"
+            );
+    }
+}
